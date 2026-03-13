e0f86a65e4b0817384d5c42aaa1fd952814be2e5
sanjay3261-wed <nallamuthusanjay4387@gmail.com>
2026-03-13 16:20:58 +0530

    Add files via upload

---
+++ fun 3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+char*digit(int a){
+    if(a%2==0)
+    return "even";
+    else
+    return "odd";
+}
+int main(){
+    int a;
+    scanf("%d",&a);
+    printf("%s",digit(a));
+}
